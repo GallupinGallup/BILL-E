0b2286ec01c4385f46a6bcb0eac1615dedac5bed
GallupinGallup <Gallupingallup@gmail.com>
2017-10-05 14:31:30 -0600

    Initial commit

---
+++ src/robot/Robot.java
@@ -0,0 +1,42 @@
+
+package robot;
+
+import edu.wpi.first.wilibj.IterativeRobot;
+
+/**
+ * The VM is configured to automatically run this class, and to call
+ * the functions corresponding to each mode, as described in the IterativeRobot
+ * Documentation. If toy change the name of this class of the package after
+ * creating this project, you must also update the manifest file in the resource
+ * @author student
+ *
+ */
+
+public class Robot extends IterativeRobot {
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any init code.
+	 */
+	public void robotInit() {
+		
+	}
+	
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void atonomousPeriodic() {
+		
+	}
+	
+	/**
+	 * this function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		
+	}
+	
+	/**
+	 *  This function is called periodically during test mode
+	 */
+
+}
